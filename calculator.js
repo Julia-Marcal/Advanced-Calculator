6d0f5415aebd05bb59f2532712f5390b43e77d1c
Julia-Marcal <anajulia.05marcal@gmail.com>
2022-10-14 11:45:05 -0300

    Adding new function that calculates an operation between integer numbers with exponents, adding semicolons in places it's needed, simplifying the code and deleting lines that are not used/useful
    -new function: calculo_expoentes
    - adding semicolons
    - simplifying some of the codes
    - deleting lines (console.log) that are not needed

--- calculator.js
+++ calculator.js
@@ -1,13 +1,13 @@
-//change num_prim if it is bigger then the inputs
-
 let Global_lmc = 1;
+// is the value of lmc that is scoped globaly
+//it was scoped that way so it can be used in other functions
 
 function lmc(n1, n2){
     lmc_tf = false;
     //while the two number aren't 1 lmc_tf continues false
     let list_p = [];
     let total_p = 1
-    //list of interger that are being used to divide the numbers
+    //list of integer that are being used to divide the numbers
     let num_prim = 2;
     // num prim
     let input1 = n1 
@@ -20,56 +20,47 @@ function lmc(n1, n2){
                 total_p = total_p*list_p[pos]
             };
             console.log('The division ended');
-            console.log(`Numbers use to calculate the lmc are: ${list_p}`);
-            console.log(`The lmc is: ${total_p}`);
+            console.log(`Numbers used to calculate the lmc are: ${list_p}`);
             Global_lmc = total_p;
-            return Global_lmc
+            lmc_tf = true;
+            return 'The lmc is: ' + Global_lmc
         }
 
         else if (num_prim != 2 && num_prim % 2 == 0 && num_prim % 3 == 0){
             //check if num_prim is a prime number
-            //if is not a prime number it will add and jump to the neinput1t number
-            console.log(`${num_prim} is not a prime number`);
+            //if is not a prime number it will add and jump to the next input number
             num_prim ++
             continue
         }
 
         else if(num_prim > input1 && num_prim > input2){
-            num_prim = 2;
             //if num_prim greater than both of the numbers that are being divided
             //reset num_prim so the numbers can be dived
+            num_prim = 2;
         };
-
-        console.log();
-        console.log(`The prime number is: ${num_prim}`);
-        console.log(`Numbers already used to divide were: ${list_p}`);
-        console.log(`Numbers that are going to be divided are: ${input1} and ${input2}`);
         
         if (input1 != 1 || input2 != 1){
-            //if input1 OR input2 are different then 2   
-            if(input1==num_prim || input2==num_prim){
+            //if input1 OR input2 are different then 1   
+            if (input1 == num_prim || input2==num_prim){
                 //if input1 OR input2 are EQUAL to the prime number
                 if (input1 == num_prim && input2 == num_prim){
                     //if the 2 inputs are EQUAL
                     list_p.push(num_prim);
-                    console.log('lmc ended');
                     break
                 }
-                else if ( input1== num_prim){
+                else if ( input1 == num_prim){
                     //if only the input1 is EQUAL  
+                    //Freeze the number that was already divided to 1
                     list_p.push(num_prim);
-                    console.log(`${input1} is equal to ${num_prim}`);
                     input1 = 1; 
-                    //Freeze the number that is already dived to 1
                     Object.freeze(input1);
                     continue
                 }
-                else if (input2==num_prim){
+                else if (input2 ==num_prim){
                     //if only the input2 is EQUAL 
+                    //Freeze the number that was already divided to 1
                     list_p.push(num_prim);
-                    console.log(`${input2} is equal to ${num_prim}`);
                     input2 = 1;
-                    //Freeze the number that is already dived to 1
                     Object.freeze(input2);
                     continue
                 }}
@@ -85,7 +76,6 @@ function lmc(n1, n2){
                     input1 = input1*num_prim; 
                     input2 = input2*num_prim;
                     //reset value of the variables because none of the numbers could be divided
-                    console.log(`Can't be dived by ${num_prim}`);
                     //change to the next prime number
                     num_prim ++;
                     continue
@@ -94,7 +84,6 @@ function lmc(n1, n2){
                     console.log('At least one of them can be divided')
                     //check if both of them can be divided
                     if(Number.isInteger(input1) && Number.isInteger(input2)){
-                        console.log('Both can be dived');
                         //add the number that was used to divide on the list 
                         list_p.push(num_prim);
                         continue
@@ -104,7 +93,6 @@ function lmc(n1, n2){
                         list_p.push(num_prim);
                         input2 = input2*num_prim;
                         //reset the other input
-                        console.log(`${input1*num_prim} can be divided`);
                         continue
                     }
                     else if(Number.isInteger(input2)){
@@ -112,7 +100,6 @@ function lmc(n1, n2){
                         list_p.push(num_prim) ;
                         input1 = input1*num_prim;
                         //reset value of the one who can't be divided
-                        console.log(`${input2*num_prim} can be divided`);
                         continue
                     } 
                 }    
@@ -125,14 +112,12 @@ function lmc(n1, n2){
                 if(Number.isInteger(input1) == true){
                     //in case the 1st value of them can be divided 
                     list_p.push(num_prim);
-                    console.log(`The first value can be divided`);
-                    console.log(input1);
                     continue
                 }
-                //in case i can't be divided
+                //in case it can't be divided
                 else{
-                    input1 = input1*num_prim;
                     //reset the value and change to the next prime number
+                    input1 = input1*num_prim;
                     num_prim++;
 
                 }
@@ -145,8 +130,6 @@ function lmc(n1, n2){
                 if(Number.isInteger(input2) == true){
                     //in case the 2nd value of them can be divided 
                     list_p.push(num_prim);
-                    console.log(`${input1} can be divided`);
-                    console.log(input2);
                     continue
                 }
                 else{
@@ -160,9 +143,10 @@ function lmc(n1, n2){
 }
 
 
-function Fraction_Operation(numerador1, denominador1, numerador2, denominador2, operation){
+function fraction_Operation(numerador1, denominador1, numerador2, denominador2, operation){
     let final_numerator = 0;
     let final_denominator = 0;
+
     if (denominador1 !=  denominador2 && operation =='+' || operation=='-'){
         //in case denominators are not the same number
         //calculate the lmc
@@ -172,17 +156,17 @@ function Fraction_Operation(numerador1, denominador1, numerador2, denominador2, 
         
         if (operation=='+'){
             //in case the operation is sum
-            console.log(`Soma de frações entre: ${numerador1}/${denominador1} + ${numerador2}/${denominador2}`)
+            console.log(`Soma de frações entre: ${numerador1}/${denominador1} + ${numerador2}/${denominador2}`);
             final_numerator = numerador1 + numerador2;
-            final_denominator = denominador1
-            console.log(final_numerator, '/',final_denominator)
+            final_denominator = denominador1;
+            return final_numerator + '/' + inal_denominator;
         }
         else if (operation=='-'){
             //if operation is minus
-            console.log(`Diferença de frações entre ${numerador1}/${denominador1} - ${numerador2}/${denominador2}`)
-            final_numerator = numerador1 - numerador2
-            final_denominator = denominador1
-            console.log(final_numerator, '/', final_denominator)
+            console.log(`Diferença de frações entre ${numerador1}/${denominador1} - ${numerador2}/${denominador2}`);
+            final_numerator = numerador1 - numerador2;
+            final_denominator = denominador1;
+            return final_numerator + '/' + final_denominator
         }
         
     }
@@ -191,41 +175,39 @@ function Fraction_Operation(numerador1, denominador1, numerador2, denominador2, 
         //if denominators are the same
         if (operation=='+'){
             //in case the operation is sum
-            console.log(`Soma de frações entre: ${numerador1}/${denominador1} + ${numerador2}/${denominador2}`)
+            console.log(`Soma de frações entre: ${numerador1}/${denominador1} + ${numerador2}/${denominador2}`);
             final_numerator = numerador1 + numerador2;
-            final_denominator = denominador1
-            console.log(final_numerator, '/',final_denominator)
+            final_denominator = denominador1;
+            return final_numerator + '/' + final_denominator;
         }
 
         else if (operation=='-'){
             //if operation is minus
-            console.log(`Diferença de frações entre: ${numerador1}/${denominador1} - ${numerador2}/${denominador2}`)
-            final_numerator = numerador1 - numerador2
-            final_denominator = denominador1
-            console.log(final_numerator, '/', final_denominator)
+            console.log(`Diferença de frações entre: ${numerador1}/${denominador1} - ${numerador2}/${denominador2}`);
+            final_numerator = numerador1 - numerador2;
+            final_denominator = denominador1;
+            return final_numerator + '/' + final_denominator
         }
 
         else if (operation== '*' || operation=='x' || operation =='X'){
             //in case the operation is multiplying
-            console.log(`Multiplicação de frações entre: ${numerador1}/${denominador1} X ${numerador2}/${denominador2}`)
-            final_numerator = numerador1 * numerador2
-            final_denominator = denominador1 * denominador2
-            console.log(final_numerator, '/' ,final_denominator )
+            console.log(`Multiplicação de frações entre: ${numerador1}/${denominador1} X ${numerador2}/${denominador2}`);
+            final_numerator = numerador1 * numerador2;
+            final_denominator = denominador1 * denominador2;
+            return final_numerator + '/' + final_denominator;
         }
         else if (operation=='/'){
             //in case the operation is diving 
-            console.log(`Divisão de fração entre: ${numerador1}/${denominador1} X ${numerador2}/${denominador2}`)
-            final_numerator = numerador1 * denominador2
-            final_denominator = numerador2 * denominador1
-            console.log(final_numerator, '/' ,final_denominator )
+            console.log(`Divisão de fração entre: ${numerador1}/${denominador1} X ${numerador2}/${denominador2}`);
+            final_numerator = numerador1 * denominador2;
+            final_denominator = numerador2 * denominador1;
+            return final_numerator + '/' + final_denominator; 
         }
     }
 }
-//console.log(Fraction_Operation(3,4,3,2,'/'))
 
 
-function Exponent_Calculator(base, exponent){
-    console.log(`The base is ${base} and the exponent is ${exponent}`)
+function exponent_Calculator(base, exponent){
     result = 1
     //result will be multiplying with base 
 
@@ -233,8 +215,50 @@ function Exponent_Calculator(base, exponent){
         //if the exponent is not equal to count 
         //result will continue to multiple with base (multiple itself)
         result *= base
+    };
+
+    return result;
+}
+
+
+function calculo_expoentes(base1, exponent1, base2, exponent2, operation_exp){
+    
+    if (base1 == base2){
+        //if bases are equal the count will be focused on the exponents
+       if (operation_exp == '-'){
+        minus_exp = (exponent_Calculator(base1,exponent1)) - (exponent_Calculator(base2,exponent2))
+        return minus_exp;
+       } 
+       else if (operation_exp == '+'){
+        sum_exp = (exponent_Calculator(base1,exponent1)) + (exponent_Calculator(base2,exponent2))
+        return sum_exp;
+       }
+
+       else if (operation_exp == '*' || operation_exp =='x' || operation_exp =='X'){
+        multiple_exp = [base1, (exponent1+exponent2)]
+        return 'base: ' + multiple_exp[0] + '\nexpoente: ' + multiple_exp[1];
+       }
+       else{
+        division_exp = [base1, (exponent1-exponent2)]
+        return 'base: ' + division_exp[0] + '\nexpoente: ' + division_exp[1] ;
+       }
     }
 
-    console.log(`The result will be ${result}`)
+    else{
+        //in case the bases imputed are different
+        diff_base1 = exponent_Calculator(base1, exponent1)
+        diff_base2 = exponent_Calculator(base2, exponent2)
+        if (operation_exp == '+'){
+            return diff_base1 + diff_base2;
+        }
+        else if (operation_exp == '-'){
+            return diff_base1 - diff_base2;
+        }
+        else if (operation_exp == '*' || operation_exp == 'x' || operation_exp == 'X'){
+            return parseInt(diff_base1 * diff_base2)
+        }
+        else{
+            return parseInt(diff_base1 / diff_base2 )
+        }
+    }
 }
-console.log(Exponent_Calculator(12,4))
